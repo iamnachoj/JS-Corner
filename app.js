de3e1056e6be8661cfbc2d00d0a941bb686f7ba2
Ignacio Jimenez <65572928+iamnachoj@users.noreply.github.com>
2021-10-17 17:20:39 +0200

    require mongoose in project

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
-
+const mongoose = require("mongoose");
 const app = express();
 
 app.set('view engine', 'ejs');
